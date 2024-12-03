045968287bee9e9057c646cb0ef59116e6e5288d
Neo <dixitrayaan@gmail.com>
2024-12-03 20:54:50 +0530

    Whatsapp Button Added

--- app/WhatsappButton.tsx
+++ app/WhatsappButton.tsx
@@ -16,7 +16,7 @@ const WhatsAppButton = () => {
 
   return (
     <a
-      href="https://wa.me/17789324598" // Replace with your WhatsApp number
+      href="https://wa.me/17788324598" // Replace with your WhatsApp number
       target="_blank"
       rel="noopener noreferrer"
       className={`fixed bottom-5 ${

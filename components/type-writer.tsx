5b5bd7b73d8328581ca4afc0d79def1ae8dbe635
Neo <dixitrayaan@gmail.com>
2025-01-07 10:42:38 +0530

    Changed

--- components/type-writer.tsx
+++ components/type-writer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 
 interface TypewriterProps {
   words: string[]
